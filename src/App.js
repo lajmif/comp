6dc289d20f88235ba96438fba085c64a40edbdce
lajmif <sy.lajmi@gmail.com>
2022-08-05 02:51:18 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Spadri from "./component/sneakers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const sneakers = [
+    {
+      id: Math.random(),
+      brand:"NIKE - air force",
+      price: "120$",
+    },
+    {
+      id: Math.random(),
+      brand:"ADIDAS - yeezy",
+      price:"369$",
+    },
+    {
+      id: Math.random(),
+      brand:"ADIDAS - run swift",
+      price:"120$",
+    },
+    {
+      id: Math.random(),
+      brand:"NIKE x Louis Vuitton",
+      price:"820$",
+    },
+  
+  ];
+
+const str ="props";
+
+const handlePrice = (name, price)=> {
+ alert(`This ${name}'s price is ${price}`)
+};
+
+ return (
+  <div className="App">
+ <Spadri spadriet={Spadri}  chaine={str} handlePrice={handlePrice}/>
+ </div>
+);
+
 }
 
 export default App;
+  
---
+++ src/component/sneakers.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const sneakers = ({spadriet, chaine, handlePrice}) => {
+  console.log(spadriet);
+  return(
+<div>
+      {
+      spadriet.map((el)=>
+        (
+          <div>
+           <h1>{el.id}</h1>
+           <h1>{el.brand}</h1>
+           <button onClick={()=> handlePrice(el.brand, el.price)}>PRICE</button>
+           </div>
+          )
+          )
+        }
+</div>
+       )
+}
+
+export default sneakers
